5c87914ef3848684dcf62ea095aa36e3e2242a8d
Shingo <shingo0620@gmail.com>
2023-04-25 15:11:22 +0800

    Update the link shell script

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm ../app/public/wp-content/plugins
-rm ../app/public/wp-content/themes
+rm -rf ../app/public/wp-content/plugins
+rm -rf ../app/public/wp-content/themes
 
 # 建立符號鏈結
-ln -fs ./public/wp-content/plugins ../app/public/wp-content/plugins
-ln -fs ./public/wp-content/themes ../app/public/wp-content/themes
+ln -fs "`pwd`/public/wp-content/plugins" ../app/public/wp-content/plugins
+ln -fs "`pwd`/public/wp-content/themes" ../app/public/wp-content/themes
