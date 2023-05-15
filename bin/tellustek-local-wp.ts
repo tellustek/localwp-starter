0f6d266bc77eb2459c68be06aabddf8e7eeb4111
Shingo <shingo0620@gmail.com>
2023-05-15 16:02:55 +0800

    Use env to setup aws region and account

--- bin/tellustek-local-wp.ts
+++ bin/tellustek-local-wp.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { TellustekLocalWpStack } from '../lib/tellustek-local-wp-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import * as dotenv from 'dotenv'
+import { TellustekLocalWpStack } from '../lib/tellustek-local-wp-stack'
+dotenv.config()
 
-const app = new cdk.App();
+const app = new cdk.App()
 new TellustekLocalWpStack(app, 'TellustekLocalWpStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -15,7 +17,7 @@ new TellustekLocalWpStack(app, 'TellustekLocalWpStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.AWS_REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+})
