c53b20ca09ed3ed6e224b4270520282b55d000c9
Shingo <shingo0620@gmail.com>
2023-05-26 15:12:14 +0800

    Update link.sh for windows

--- link.sh
+++ link.sh
@@ -2,6 +2,12 @@
 rm -rf ../app/public/wp-content/plugins
 rm -rf ../app/public/wp-content/themes
 
-# 建立符號鏈結
-ln -fs "`pwd`/public/wp-content/plugins" ../app/public/wp-content/plugins
-ln -fs "`pwd`/public/wp-content/themes" ../app/public/wp-content/themes
+windows() { [[ -n "$WINDIR" ]]; }
+
+if windows; then
+  cmd <<< "mklink /j \"../app/public/wp-content/plugins\" \"public/wp-content/plugins\"" > /dev/null
+  cmd <<< "mklink /j \"../app/public/wp-content/themes\" \"public/wp-content/themes\"" > /dev/null
+else
+  ln -fs "`pwd`/public/wp-content/plugins" ../app/public/wp-content/plugins
+  ln -fs "`pwd`/public/wp-content/themes" ../app/public/wp-content/themes
+fi
